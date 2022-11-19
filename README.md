cdb301e8bd5169a650e0b3a92f84764b1ca78d5c
dnogl <frederett@teknologiumum.com>
2022-11-19 18:52:42 +0700

    feature: work on collision map layer

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 ## Haxeflixel x LDtk
 
-This a game example build using Haxeflixel and LDtk.
+This a game example build using Haxeflixel and LDtk, not final example but at last working.
+
+TODO:
+
+- [ ] Load collision layer effectivly without touching assets.
+- [ ] Collision shape for player sprite.
+- [ ] Point system.
+- [ ] Better menu.
+
+### LDtk Configuration
+
+1. In this example I use super simple export mode.
+2. For making collision layer use Integer Grid Layer, and load graphic file that used as tileset for the map.
+    - See [PlayState.hx at line 21](./source/states/PlayState.hx)
+    - Reference: https://www.ohsat.com/tutorial/flixel/using-ldtk-with-flxtilemap/index.php
 
 ### License
 
-Code: Unlicense
-Assets: CC0
+- Code: Unlicense
+- Assets: CC0
--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1,17 +1,23 @@
 package states;
 
+import flixel.FlxG;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.tile.FlxTilemap;
 import ldtk.Level;
 import sprites.PlayeSprite.Player;
+import utils.LDtkUtils.gridTilesToTilemap;
 
 class PlayState extends FlxState
 {
+	var collisionMap:FlxTilemap;
+	var player:Player;
+
 	override public function create()
 	{
 		super.create();
 
+		// This for load the LDtk map class
 		var ldtkMap = new LevelMap();
 
 		// Iterate all world levels
@@ -29,13 +35,22 @@ class PlayState extends FlxState
 			if (level.hasBgImage())
 				container.add(level.getBgSprite());
 
+			var terrain = level.l_Terrain;
+
+			var csv = level.l_IntGrid.json.intGridCsv;
+
+			collisionMap = new FlxTilemap();
+			// TODO: still work in this for how efficient load collision layer.
+			// collisionMap.loadMapFromArray(csv, 8, 8, AssetPaths.IntGrid_int__png, 32, 32);
+			// collisionMap.loadMapFromArray(csv, 320, 320, AssetPaths.cyber_world_terain__png, 32, 32, AUTO, 0, 1, 2);
+			collisionMap.loadMapFromCSV(AssetPaths.IntGrid__csv, AssetPaths.Terrain__png, 32, 32);
 			// Render layer "Background"
-			level.l_Terrain.render(container);
 
-			var playerPosition = level.l_PlayerPosition;
+			terrain.render(container);
 
-			var player:Player;
-			player = new Player(playerPosition.pxTotalOffsetX, playerPosition.pxTotalOffsetY);
+			var playerPosition = level.l_PlayerPosition.all_Player[0];
+
+			player = new Player(32, 32);
 
 			container.add(player);
 		}
@@ -44,5 +59,6 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		FlxG.collide(player, collisionMap);
 	}
 }
